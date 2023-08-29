a9c1dab47425dad9a81dae70907c7cc8067fa009
Nabil Muhammad Raihan <namura152@gmail.com>
2023-08-30 06:08:28 +0700

    add file resource local_restaurant.json

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          Text("Restaurant"),
-          Text("Recommendation restaurant for you!")
-        ],
-      ),
-    );
+    return MaterialApp();
   }
 }
