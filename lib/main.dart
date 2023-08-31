7f253a98d3dbc8602529c4d79a9f91efbaaa6686
Nabil Muhammad Raihan <namura152@gmail.com>
2023-08-31 10:24:08 +0700

    tambah file restaurant_list_page.dart dan pindahin widget dari main.dart ke restaurant_list_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_app/data/model/restaurant.dart';
+import 'package:restaurant_app/ui/restaurant_list_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,51 +19,6 @@ class MyApp extends StatelessWidget {
       routes: {
         RestaurantListPage.routeName: (context) => const RestaurantListPage()
       },
-    );
-  }
-}
-
-class RestaurantListPage extends StatelessWidget {
-  static const routeName = '/restaurant_list';
-
-  const RestaurantListPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Restaurant App",
-          style: TextStyle(fontSize: 25),
-        ),
-        toolbarHeight: 170,
-      ),
-      body: FutureBuilder<String>(
-        future: DefaultAssetBundle.of(context)
-            .loadString("assets/local_restaurant.json"),
-        builder: (context, snapshot) {
-          final List<Restaurant> restaurants = parseRestaurants(snapshot.data);
-          return ListView.builder(
-            itemCount: restaurants.length,
-            itemBuilder: (context, index) {
-              return _buildRestaurantItem(context, restaurants[index]);
-            },
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildRestaurantItem(BuildContext context, Restaurant restaurant) {
-    return ListTile(
-      contentPadding:
-          const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      leading: Image.network(
-        restaurant.pictureId,
-        width: 100,
-        errorBuilder: (ctx, error, _) => const Center(child: Icon(Icons.error)),
-      ),
-      title: Text(restaurant.name),
     );
   }
 }
