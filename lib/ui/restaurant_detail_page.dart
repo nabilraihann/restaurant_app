48798f0b5baa9d71948ace3aa75ba1dff7659592
Nabil Muhammad Raihan <namura152@gmail.com>
2023-08-31 13:52:35 +0700

    tambah detail restoran di restaurant_detail_page.dart

--- lib/ui/restaurant_detail_page.dart
+++ lib/ui/restaurant_detail_page.dart
@@ -20,7 +20,99 @@ class RestaurantDetailPage extends StatelessWidget {
           child: Column(
             children: [
               Image.network(restaurant.pictureId),
-              Text(restaurant.name)
+              Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              restaurant.name,
+                              style: const TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              children: [
+                                const Icon(Icons.location_pin, size: 20),
+                                Text(restaurant.city),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                Text(restaurant.rating.toString()),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Divider(
+                        color: Colors.grey,
+                      ),
+                      const Text(
+                        'Description',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        restaurant.description,
+                        overflow: TextOverflow.clip,
+                        textAlign: TextAlign.justify,
+                      ),
+                      Divider(
+                        color: Colors.blueGrey,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Menus:'),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Foods:'),
+                                Text(
+                                  restaurant.menus.foods
+                                      .map((foods) => foods.name)
+                                      .toString(),
+                                ),
+                                Text('Drinks:'),
+                                Text(
+                                  restaurant.menus.drinks
+                                      .map((drink) => drink.name)
+                                      .toString(),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ))
             ],
           ),
         ),
